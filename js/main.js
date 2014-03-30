00c8745af461c8455b53a8dcda7fb8d6d2079b5f
Cassidy Williams <cassidoo@outlook.com>
2014-03-30 00:10:53 -0500

    color changes and prettier progress bars

--- js/main.js
+++ js/main.js
@@ -4,8 +4,31 @@ function populateWeeks()
 {
     for(var i = 0; i < 32; i++)
     {
-        weekNums[i] = i+1; 
+        weekNums[i] = i + 1;
         document.getElementById("weeks").innerHTML += "<span>" + weekNums[i] + "</span>";
-    } 
+    }
 }
 
+function setProgress(id, level)
+{
+    var bar = document.getElementById(id);
+
+    bar.innerHTML = '<div class="level"><div class="bub"></div></div>';
+
+    var levelBar = $("#" + id + " .level");
+
+    levelBar.css('width', level + '%');
+
+    if(level > 65)
+    {
+        levelBar.addClass("high");
+    }
+    else if(level >= 35)
+    {
+        levelBar.addClass("medium");
+    }
+    else
+    {
+        levelBar.addClass("low");
+    }
+}
